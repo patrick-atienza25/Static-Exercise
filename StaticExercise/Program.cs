5ed5a3e76932ad8404d043b816d913a71fb6711c
patrick-atienza25 <patienza25@gmail.com>
2022-11-01 11:26:49 -0700

    finished exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace StaticExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(99);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(21);
+            Console.WriteLine($"99 degrees fahrenheit converted to celsius is {celsius}.");
+            Console.WriteLine();
+            Console.WriteLine($"21 degrees celsius converted to fahrenheit is {fahrenheit}");
         }
     }
 }
